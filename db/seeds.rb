03d0fa3b5218b95726d1a61413f15c651077b717
Winston Pemberton <winstonpemberton@gmail.com>
2020-09-17 23:06:03 +0000

    added more pictures and it seems that attaching pictures worked since I didn't get any errors will test later

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Weapon.create(name: 'Dull Sword', damage: 8 )
-Weapon.create(name: 'Epic Sword', damage: 15 )
-Weapon.create(name: 'Dawnbreaker', damage: 25 )
+@weapon1 = Weapon.create(name: 'Steel Sword', damage: 8 )
+@weapon1.picture.attach(io: File.open('app/assets/images/weapons/steel sword.PNG'),
+filename: 'steel sword.PNG')
 
-Armor.create(name: 'Light Armor', armor_raiting: 5 )
-Armor.create(name: 'Medium Armor', armor_raiting: 10 )
-Armor.create(name: 'Dragonbone Armor', armor_raiting: 15 )
+@weapon2 = Weapon.create(name: 'Orcish Sword', damage: 15 )
+@weapon2.picture.attach(io: File.open('app/assets/images/weapons/orcish sword.PNG'),
+filename: 'orcish sword.PNG')
 
-Potion.create(name: 'Basic Healing Potion', heal_amount: 10 )
-Potion.create(name: 'Superior Healing Potion', heal_amount: 25 )
-Potion.create(name: 'Full Heal Potion', heal_amount: 50 )
+@weapon3 = Weapon.create(name: 'Dawnbreaker', damage: 25 )
+@weapon3.picture.attach(io: File.open('app/assets/images/weapons/dawnbreaker.PNG'),
+filename: 'dawnbreaker.PNG')
+#
+# Armor.create(name: 'Light Armor', armor_raiting: 5 )
+# Armor.create(name: 'Medium Armor', armor_raiting: 10 )
+# Armor.create(name: 'Dragonbone Armor', armor_raiting: 15 )
+#
+# Potion.create(name: 'Basic Healing Potion', heal_amount: 10 )
+# Potion.create(name: 'Superior Healing Potion', heal_amount: 25 )
+# Potion.create(name: 'Full Heal Potion', heal_amount: 50 )
