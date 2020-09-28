50d0b728dbf80ef47a7fa3a826b49551d2c6e5d3
Winston Pemberton <winstonpemberton@gmail.com>
2020-09-28 22:02:24 +0000

    startng to code for battling

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -11,8 +11,12 @@ class GamesController < ApplicationController
   end
 
   def battle
-    @game = Game.find(params[:id])
-    @enemy = Enemy.find(1)
+    @enemy = Enemy.create(name: "Big Bad Demon", damage: 5, health: 20)
+    @character = current_character
+  end
+
+  def use_potion
+
   end
 
   def boss_battle
@@ -27,7 +31,16 @@ class GamesController < ApplicationController
   end
 
   def update_battle
+    character = Character.find(params[:character_id])
+    enemy = Enemy.find(params[:enemy_id])
 
+    if character
+      response = character.attack(enemy)
+      flash[:notice] = response
+      redirect_to user_character_path(current_character)
+    else
+      redirect_to merchant_path(@merchant)
+    end
   end
 
   def create
@@ -35,7 +48,7 @@ class GamesController < ApplicationController
     current_user.set_current_game(@game)
 
     if current_user
-      @character = Character.create(user_id: current_user.id, game_id: @game.id,:health 50, character_params)
+      @character = Character.create(user_id: current_user.id, game_id: @game.id,health: 50, character_params)
       Inventory.create(character_id: @character.id)
       @game.set_current_character(@character)
       @merchant = Merchant.create( game_id: @game.id, merchant_params)
